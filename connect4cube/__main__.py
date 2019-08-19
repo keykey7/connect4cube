b9bf754ecea3a603b49a9aeff406ee7135d95182
sst6z <28892039+sst6z@users.noreply.github.com>
2019-08-19 20:18:17 +0200

    conditional import of raspberry pi specific modules

--- connect4cube/__main__.py
+++ connect4cube/__main__.py
@@ -3,23 +3,24 @@ import sys
 
 from connect4cube.game import Game
 from connect4cube.player import RandomPlayer, StdinPlayer
-from connect4cube.player_gpio import GpioPlayer
-from connect4cube.viewer_led import LedViewer
+
+is_a_raspberry = False
+try:
+    import RPi.GPIO  # noqa: F401
+    is_a_raspberry = True
+except (ImportError, RuntimeError):
+    pass
+
+if is_a_raspberry:
+    from connect4cube.player_gpio import GpioPlayer
+    from connect4cube.viewer_led import LedViewer
 
 logger = logging.getLogger(__name__)
 logger.debug("sys.path=" + ":".join(sys.path))
 
 
-def is_a_raspberry():
-    try:
-        import RPi.GPIO  # noqa: F401
-        return True
-    except (ImportError, RuntimeError):
-        return False
-
-
 def main():
-    if is_a_raspberry():
+    if is_a_raspberry:
         player = GpioPlayer()
         player.play_both_sides = True
         Game(player, player, viewer=LedViewer).play()
